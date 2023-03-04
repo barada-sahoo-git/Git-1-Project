5dfd2b0f7c298b38e915342d061469ab70a445c9
barada-sahoo-git <barada.tech@gmail.com>
2023-03-04 18:35:43 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("hello man, how are you");
+        System.out.println("this is dev branch");
+    }
+}
